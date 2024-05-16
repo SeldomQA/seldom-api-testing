b3fd03bd2e1f39832fec28767c726dfa9c3df4ae
defnngj <defnngj@gmail.com>
2024-05-16 18:29:03 +0800

    feat: add run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+import seldom
+
+if __name__ == '__main__':
+    seldom.main(
+        path="./test_dir",  # 运行用例目录
+        base_url="https://httpbin.org",  # 基础URL地址
+        rerun=3   # 重跑次数
+    )
